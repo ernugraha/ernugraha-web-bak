e127a541d25c42502a748ba7dc58ed4bced38711
ernugraha <138465397+ernugraha@users.noreply.github.com>
2026-07-04 09:16:42 +0700

    fix

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -117,31 +117,30 @@ let BeautifulJekyllJS = {
     }
   },
 
-initSearch : function() {
-  if (!document.getElementById("beautifuljekyll-search-overlay")) {
-    return;
-  }
+  initSearch : function() {
+    if (!document.getElementById("beautifuljekyll-search-overlay")) {
+      return;
+    }
 
-  $("#nav-search-link").click(function(e) {
-    e.preventDefault();
-    $("#beautifuljekyll-search-overlay").show();
-    $("#nav-search-input").focus().select();
-    $("body").addClass("overflow-hidden");
-  });
-
-  $("#nav-search-exit").click(function(e) {
-    e.preventDefault();
-    $("#beautifuljekyll-search-overlay").hide();
-    $("body").removeClass("overflow-hidden");
-  });
-
-  $(document).on("keyup", function(e) {
-    if (e.key === "Escape") {
+    $("#nav-search-link").click(function(e) {
+      e.preventDefault();
+      $("#beautifuljekyll-search-overlay").show();
+      $("#nav-search-input").focus().select();
+      $("body").addClass("overflow-hidden");
+    });
+    $("#nav-search-exit").click(function(e) {
+      e.preventDefault();
       $("#beautifuljekyll-search-overlay").hide();
       $("body").removeClass("overflow-hidden");
-    }
-  });
-}
+    });
+    $(document).on('keyup', function(e) {
+      if (e.key == "Escape") {
+        $("#beautifuljekyll-search-overlay").hide();
+        $("body").removeClass("overflow-hidden");
+      }
+    });
+  }
+};
 
 // 2fc73a3a967e97599c9763d05e564189
 
